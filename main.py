620b0813e16874503e10fb38108bc9a5403e2083
huyenhocvt <hanhangtruong@gmail.com>
2025-05-08 14:52:56 +0700

    Cập nhật 3 roiter: Trang chủ, Giờ hiện tại, Độ Nhà

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, send_file, jsonify
 from datetime import datetime
 import os
-from xuat_so_do_nha import xu_ly_dau_vao_va_xuat_file
+from xuat_so_do_nha import xuat_so_do
 
 app = Flask(__name__)
 
@@ -18,8 +18,12 @@ def get_time():
 def do_nha():
     if request.method == 'POST':
         dulieu = request.form['dulieu']
-        file_path = xu_ly_dau_vao_va_xuat_file(dulieu)
-        return send_file(file_path, as_attachment=True)
+        try:
+            toa_do, ten_nguoi_dung = [x.strip() for x in dulieu.split(',', 1)]
+            file_path = xuat_so_do(toa_do, ten_nguoi_dung)
+            return send_file(file_path, as_attachment=True)
+        except Exception as e:
+            return f"Lỗi xử lý: {e}"
     return render_template('do_nha_input.html')
 
 if __name__ == '__main__':
