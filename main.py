81db96579a2a77de869985c7368812d2cb1878aa
huyenhocvt <hanhangtruong@gmail.com>
2025-05-14 12:04:02 +0700

    Đăng ký đầy đủ các route theo menu trái

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
+from roiter.do_nha import do_nha_bp
+from roiter.lap_cong_viec import lap_cong_viec_bp
+from roiter.them_nguoi_moi import them_nguoi_moi_bp
+from roiter.time_checkbox import time_checkbox_bp
 
 app = Flask(__name__)
-app.secret_key = "seri_tien_secret"
+app.secret_key = "huyen_hoc_secret"
+
+# Đăng ký các blueprint
+app.register_blueprint(do_nha_bp)
+app.register_blueprint(lap_cong_viec_bp)
+app.register_blueprint(them_nguoi_moi_bp)
+app.register_blueprint(time_checkbox_bp)
 
 @app.route("/")
 def index():
